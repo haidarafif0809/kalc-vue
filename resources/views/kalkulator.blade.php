b50aa09df4e0e38699a04a86271f0905ab06a6bc
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-11-10 13:38:46 +0700

    Initial Commit

---
+++ resources/views/kalkulator.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Kalc Vue</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+	<div id="app">
+		<kalkulator></kalkulator>
+	</div>
+
+	<script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/vue.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Belajar Vue</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+	<div id="app">
+		<example></example>
+	</div>
+
+	<script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
+</body>
+</html>
